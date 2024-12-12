9a55755addd8db2bbd4552468d5c6246789c2e5f
Manas Sharma <manas.sharma2084@gmail.com>
2024-12-12 17:10:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-gsap.from("#img1",{
-        opacity:0,
-        duration:1,
-        delay:0.4,
-        y:60,
-    })
+gsap.registerPlugin(ScrollTrigger);
 
-    gsap.from("#img2",{
-        opacity:0,
-        duration:1,
-        delay:0.4,
-        y:-60,
-    })
+gsap.from("#img1", {
+    opacity: 0,
+    duration: 1,
+    delay: 0.4,
+    y: 60,
+});
 
-    gsap.from("#img3",{
-        opacity:0,
-        duration:1,
-        delay:0.4,
-        x:60,
-    })
+gsap.from("#img2", {
+    opacity: 0,
+    duration: 1,
+    delay: 0.4,
+    y: -60,
+});
 
-    gsap.from("#main>h1",{
-        opacity:0,
-        duration:1,
-        delay:0.1,
-        x:60, 
-    })
+gsap.from("#img3", {
+    opacity: 0,
+    duration: 1,
+    delay: 0.4,
+    x: 60,
+});
+
+gsap.from("#main>h1", {
+    opacity: 0,
+    duration: 1,
+    delay: 0.1,
+    x: 60,
+});
+
+gsap.from("#page2 h5, #page2>h1, #page2>#aboutus", {
+    opacity: 0,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: "#page2", // Use the entire container as trigger
+        scroller: "body",
+        start: "top 50%",
+    },
+});
+
